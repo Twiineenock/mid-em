94e3952bdbda9039dde7638a0d6a9e276abcbec7
Twiine Enock <twiineenockfox@gmail.com>
2023-11-01 12:41:41 +0300

    fox change

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        // Update the assetFileNames pattern without the "[query]" placeholder
+        assetFileNames: 'assets/ASSETS/[name]-[hash][ext]',
+      },
+    },
+  },
 })
+
